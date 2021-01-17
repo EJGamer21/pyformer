253e3c2278713c819a84be754084b6efac1c9f87
Enger Jimenez <ejimenezr21@gmail.com>
2021-01-17 15:56:19 -0400

    Updated game_manager.py

--- src/game_manager.py
+++ src/game_manager.py
@@ -1,5 +1,5 @@
-import pygame, sys
-
+import pygame
+import sys
 
 class GameManager():
     clock = None
@@ -7,20 +7,25 @@ class GameManager():
     def __init__(self):
         pygame.init()
 
-    def init_screen(self, title, screen_size,):
+    def init_screen(self, title, screen_size) -> pygame.Surface:
         pygame.display.set_caption(title)
         self.clock = pygame.time.Clock()
 
         return pygame.display.set_mode(screen_size)
 
+    def update_screen(self):
+        pygame.display.update()
+
     def quit(self):
         pygame.quit()
         sys.exit()
 
+    def tick(self, fps):
+        self.clock.tick(fps)
+
+
     def check_quit(self):
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if (event.type == pygame.QUIT
+            or (event.type == pygame.KEYUP and event.key == pygame.K_ESCAPE)):
                 quit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    quit()
